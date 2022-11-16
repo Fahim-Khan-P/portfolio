ddb5bfa3d37a092a7f63ca58f7e2581dc2e2b81d
Mukama Steven <stevenmukama1@gmail.com>
2022-11-16 12:47:12 +0200

    Added changes in index.js

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const nav = document.getElementsByClassName('nav');
 const mobileNav = document.getElementsByClassName('mobile-nav');
 const humberger = document.getElementById('humberger');
 const close = document.getElementById('closemenu');
+const porfolioLink = document.getElementById('portfolioLink');
+const aboutLink = document.getElementById('aboutLink');
+const contactLink = document.getElementById('contactLink');
+
+porfolioLink.addEventListener('click', () => {
+  mobileNav[0].classList.remove('open');
+  nav[0].classList.remove('close');
+  mobileNav[0].classList.add('close');
+  nav[0].classList.add('openFlex');
+});
+aboutLink.addEventListener('click', () => {
+  mobileNav[0].classList.remove('open');
+  nav[0].classList.remove('close');
+  mobileNav[0].classList.add('close');
+  nav[0].classList.add('openFlex');
+});
+contactLink.addEventListener('click', () => {
+  mobileNav[0].classList.remove('open');
+  nav[0].classList.remove('close');
+  mobileNav[0].classList.add('close');
+  nav[0].classList.add('openFlex');
+});
 
 humberger.addEventListener('click', () => {
   mobileNav[0].classList.remove('close');
