48227338678defbb00763a1b5ffc63a9e599c8a2
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2022-11-16 13:45:31 +0430

    Correct JS linter erorrs

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const nav = document.getElementsByClassName("nav");
-const mobileNav = document.getElementsByClassName("mobile-nav");
-const humberger = document.getElementById("humberger");
-const close = document.getElementById("closemenu");
-const main = document.getElementById("port");
-const moblieMenuList = document.getElementsByClassName("moblie-menu-list");
+const nav = document.getElementsByClassName('nav');
+const mobileNav = document.getElementsByClassName('mobile-nav');
+const humberger = document.getElementById('humberger');
+const close = document.getElementById('closemenu');
 
-humberger.addEventListener("click", () => {
-  mobileNav[0].classList.remove("close");
-  nav[0].classList.remove("openFlex");
-  mobileNav[0].classList.add("open");
-  nav[0].classList.add("close");
+humberger.addEventListener('click', () => {
+  mobileNav[0].classList.remove('close');
+  nav[0].classList.remove('openFlex');
+  mobileNav[0].classList.add('open');
+  nav[0].classList.add('close');
 });
-close.addEventListener("click", () => {
-  mobileNav[0].classList.remove("open");
-  nav[0].classList.remove("close");
-  mobileNav[0].classList.add("close");
-  nav[0].classList.add("openFlex");
+close.addEventListener('click', () => {
+  mobileNav[0].classList.remove('open');
+  nav[0].classList.remove('close');
+  mobileNav[0].classList.add('close');
+  nav[0].classList.add('openFlex');
 });
-
-for (var element of moblieMenuList) {
-  element.addEventListener("click", () => {
-    mobileNav[0].classList.remove("open");
-    nav[0].classList.remove("close");
-    mobileNav[0].classList.add("close");
-    nav[0].classList.add("openFlex");
-  });
-}
